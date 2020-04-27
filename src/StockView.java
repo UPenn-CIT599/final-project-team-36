5a4e129d65fd3f229b4ba8689b3a3daa4db83c54
Chase <chasehuc@seas.upenn.edu>
2020-04-27 18:05:23 -0400

    user option update

--- src/StockView.java
+++ src/StockView.java
@@ -98,7 +98,7 @@ public class StockView {
         System.out.println("Please enter the option on how you would like to proceed:");
         System.out.println("1: Choose an individual stock to view");
         System.out.println("2: Show Stock line chart");
-        System.out.println("3: Print out stock price daily history");
+        System.out.println("3: Print out stock price history");
         // if (whichStock.equals("S&P 500")) {
         System.out.println("4: View the price predictor for " + whichStock);
         // }
